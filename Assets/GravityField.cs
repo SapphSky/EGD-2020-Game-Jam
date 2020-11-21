1f745bbd179fbb3526fd888f6e77ed878dcaefbf
SapphSky <44979895+SapphSky@users.noreply.github.com>
2020-11-21 11:53:53 -0500

    Added lamp entities

--- Assets/GravityField.cs
+++ Assets/GravityField.cs
@@ -3,11 +3,27 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class GravityField : MonoBehaviour {
-	public Light light;
-	public LayerMask layerMask;
-	private void Update() {
+	public Transform rootField;
+	public float force = 0.5f;
+	private void OnTriggerStay(Collider other) {
+		
+		if (other.GetComponent<Rigidbody>()) {
+			Rigidbody otherBody = other.GetComponent<Rigidbody>();
+			RaycastHit hit;
 
-		RaycastHit hit;
-		Physics.Raycast(transform.position, transform.forward, out hit, light.range, layerMask, QueryTriggerInteraction.Ignore);
+			// Test to see that the closest point on the object is at least within line of sight of the lamp's cast.
+			if (Physics.Raycast(rootField.position, other.ClosestPoint(rootField.position) - rootField.position, out hit)) {
+				// Debug.DrawLine(rootField.position, other.ClosestPoint(rootField.position) - rootField.position, Color.blue, 1.0f, true);
+				otherBody.useGravity = false;
+				otherBody.AddForce(Vector3.up * force, ForceMode.VelocityChange);
+			}
+		}
+	}
+
+	private void OnTriggerExit(Collider other) {
+		if (other.GetComponent<Rigidbody>()) {
+			Rigidbody otherBody = other.GetComponent<Rigidbody>();
+			otherBody.useGravity = true;
+		}
 	}
 }
